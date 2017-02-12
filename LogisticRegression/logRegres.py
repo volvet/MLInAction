ba34ba3fcda06bb8ebc3ff9755f54a1030a2143b
volvet <qizh@cisco.com>
2017-02-12 10:41:22 +0800

    plot best fitness

--- LogisticRegression/logRegres.py
+++ LogisticRegression/logRegres.py
@@ -1,5 +1,7 @@
 
 from numpy import *
+import matplotlib.pyplot as plt
+import sys
 
 def loadDataSet():
 	dataMat = []
@@ -26,12 +28,42 @@ def gradAscent(dataMatIn, classLabels):
 	for k in range(maxCycles):
 		h = sigmoid(dataMatrix * weights)
 		error = labelMat - h
-		weights = weights + alpha * dataMatrix.transpose() * error;
+		weights = weights + alpha * dataMatrix.transpose() * error
 	return weights
 
 
+def plotBestFilt(weights):
+	dataMat, labelMat = loadDataSet()
+	dataArr = array(dataMat)
+	n = shape(dataArr)[0]
+	xcord1 = []
+	ycord1 = []
+	xcord2 = []
+	ycord2 = []
+	for i in range(n):
+		if int(labelMat[i]) == 1:
+			xcord1.append(dataArr[i, 1])
+			ycord1.append(dataArr[i, 2])
+		else:
+			xcord2.append(dataArr[i, 1])
+			ycord2.append(dataArr[i, 2])
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+	ax.scatter(xcord2, ycord2, s=30, c='green')
+    
+	x = arange(-4.0, 4.0, 0.1)
+	y = (-weights[0] - weights[1]*x)/weights[2]
+	ax.plot(x, y)
+	plt.xlabel('X1')
+	plt.ylabel('Y1')
+	plt.show()
+
+
+
 if __name__ == '__main__':
+	sys.setrecursionlimit(1500)
 	dataAttr, labelMat = loadDataSet()
-	weights = gradAscent(dataAttr, labelMat);
-	print weights;
+	weights = gradAscent(dataAttr, labelMat)
+	plotBestFilt(weights.getA())
 
