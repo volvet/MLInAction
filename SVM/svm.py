298304a462937f62557c079b6f000a43d4456937
volvet <qizh@cisco.com>
2017-02-24 21:38:56 +0800

    try simple smo

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,4 +1,6 @@
 
+from numpy import *
+
 def loadDataSet(fileName):
     dataMat = []; labelMat = []
     fr = open(fileName)
@@ -23,7 +25,31 @@ def clipAlpha(aj, H, L):
 	return aj
 
 
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+	dataMatrix = mat(dataMatIn)
+	labelMat = mat(classLabels).transpose()
+	b = 0
+	m, n = shape(dataMatIn)
+	alphas = mat(zeros((m, 1)))
+
+	#print labelMat
+	#print alphas
+	iter = 0
+	while( iter < maxIter ):
+		alphaPairsChanged = 0
+		for i in range(m):
+		    fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i,:].T)) + b
+		    print fXi
+		    Ei = fXi - float(labelMat[i])
+		    if (labelMat[i]*Ei < -toler) and (alphas[i] < C) or ( (labelMat[i]*Ei > toler) and (alphas[i] > 0) ):
+		    	j = selectJrand(i, m)
+
+		iter = maxIter
+
+
 if __name__ == '__main__':
     dataMat, labelMat = loadDataSet('testSet.txt')
-    print dataMat
-    print labelMat
+    #print dataMat
+    #print labelMat
+
+    smoSimple(dataMat, labelMat, 0.6, 0.001, 40)
